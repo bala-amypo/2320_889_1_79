df6c76b17b9e77c95b0e396aac393493507ecf39
bala-amypo <bala@amypo.in>
2025-12-17 08:18:21 +0000

    User commit

--- src/main/java/com/example/demo/entity/newentity/NewFileEntity.java
+++ src/main/java/com/example/demo/entity/newentity/NewFileEntity.java
@@ -11,5 +11,40 @@ public class NewFileEntity{
     @NotBlank(message = "no blank allowed")
     @Email(message ="invalid format")
     private String email;
+    public NewFileEntity(Long id,@NotBlank(message="should not contain spaces") String name,@NotBlank(message ="no blank allowed")
+    @Email(message = "Invalid format") String email)
+    {
+          this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+   
+    public String getName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
+    public NewFileEntity() {
+    }
     
+
+    
+    
+    
+
+}
 }
