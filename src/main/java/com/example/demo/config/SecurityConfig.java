114d46d66407e176819d671c5951b112a6586665
bala-amypo <bala@amypo.in>
2025-12-18 18:19:13 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,5 +19,11 @@ public class SecurityConfig {
             .httpBasic(basic -> basic.disable());
 
         return http.build();
+    }
+
+    // ⭐ THIS FIXES YOUR ERROR ⭐
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
