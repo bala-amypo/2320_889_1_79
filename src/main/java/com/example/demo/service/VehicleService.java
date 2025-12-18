92c1bf129fbaef5504ce9e05b3a578d2b6d8e494
bala-amypo <bala@amypo.in>
2025-12-18 17:49:20 +0000

    User commit

--- src/main/java/com/example/demo/service/VehicleService.java
+++ src/main/java/com/example/demo/service/VehicleService.java
@@ -2,6 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.entity.Vehicle;
 
+import java.util.List;
+
 public interface VehicleService {
-    Vehicle saveVehicle(Vehicle vehicle);
+
+    Vehicle addVehicle(Long userId, Vehicle vehicle);
+
+    List<Vehicle> getVehiclesByUser(Long userId);
+
+    Vehicle findById(Long vehicleId);
 }
