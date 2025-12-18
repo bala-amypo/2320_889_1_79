51b1eb34d205934835c2a383392314cb938669a3
bala-amypo <bala@amypo.in>
2025-12-18 08:40:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -10,4 +10,65 @@ public class User{
     private String ADMIN;
     private String USER;
 
+    package com.example.demo.entity;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String ADMIN;
+    private String USER;
+
+    public User(Long id, String name, String email, String aDMIN, String uSER) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        ADMIN = aDMIN;
+        USER = uSER;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getADMIN() {
+        return ADMIN;
+    }
+
+    public void setADMIN(String aDMIN) {
+        ADMIN = aDMIN;
+    }
+
+    public String getUSER() {
+        return USER;
+    }
+
+    public void setUSER(String uSER) {
+        USER = uSER;
+    }
 }
+
