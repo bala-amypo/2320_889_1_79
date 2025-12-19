b975836887d12859cccdf468e8c763ac4e33d115
bala-amypo <bala@amypo.in>
2025-12-19 09:14:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Vehicle.java
+++ src/main/java/com/example/demo/entity/Vehicle.java
@@ -3,69 +3,18 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "vehicles")
 public class Vehicle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String model;
-    private String number;
-
-    private double capacityKg;
+    private String vehicleNumber;
+    private Double fuelEfficiency;
+    private Double capacityKg;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private User user;
 
-    public Vehicle() {
-    }
-
-    public Vehicle(String model, String number, double capacityKg, User user) {
-        this.model = model;
-        this.number = number;
-        this.capacityKg = capacityKg;
-        this.user = user;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public double getCapacityKg() {
-        return capacityKg;
-    }
-
-    public void setCapacityKg(double capacityKg) {
-        this.capacityKg = capacityKg;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
+    // getters & setters
 }
