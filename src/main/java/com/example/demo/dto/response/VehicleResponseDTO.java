d3eff2a767395420951d72d243306999c5e92da1
bala-amypo <bala@amypo.in>
2025-12-19 08:31:45 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/response/VehicleResponseDTO.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto.response;
+
+public class VehicleResponseDTO {
+
+    private Long id;
+    private String vehicleNumber;
+    private Double capacityKg;
+    private Double fuelEfficiency;
+
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVehicleNumber() {
+        return vehicleNumber;
+    }
+    
+    public void setVehicleNumber(String vehicleNumber) {
+        this.vehicleNumber = vehicleNumber;
+    }
+
+    public Double getCapacityKg() {
+        return capacityKg;
+    }
+    
+    public void setCapacityKg(Double capacityKg) {
+        this.capacityKg = capacityKg;
+    }
+
+    public Double getFuelEfficiency() {
+        return fuelEfficiency;
+    }
+    
+    public void setFuelEfficiency(Double fuelEfficiency) {
+        this.fuelEfficiency = fuelEfficiency;
+    }
+}
