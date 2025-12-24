bab9cd18f5a722b4957ec2337ce2692cb039177a
bala-amypo <bala@amypo.in>
2025-12-24 05:57:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/RouteOptimizationResultRepository.java
+++ src/main/java/com/example/demo/repository/RouteOptimizationResultRepository.java
@@ -2,8 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.RouteOptimizationResult;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RouteOptimizationResultRepository extends JpaRepository<RouteOptimizationResult, Long> {
 }
