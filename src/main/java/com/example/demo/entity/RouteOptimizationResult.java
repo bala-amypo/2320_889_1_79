668e27034f3a52b1c3a150c8fc17655c2d3079d9
bala-amypo <bala@amypo.in>
2025-12-24 20:05:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/RouteOptimizationResult.java
+++ src/main/java/com/example/demo/entity/RouteOptimizationResult.java
@@ -1,35 +1,28 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name="route_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RouteOptimizationResult {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // TEST EXPECTS THESE FIELDS
     private double totalDistance;
     private double estimatedTime;
+
     private LocalDateTime generatedAt;
 
     @OneToOne
+    @JoinColumn(name = "shipment_id")
     private Shipment shipment;
-
-    public Long getId(){ return id;}
-    public void setId(Long id){ this.id=id;}
-
-    public double getTotalDistance(){ return totalDistance;}
-    public void setTotalDistance(double totalDistance){ this.totalDistance=totalDistance;}
-
-    public double getEstimatedTime(){ return estimatedTime;}
-    public void setEstimatedTime(double estimatedTime){ this.estimatedTime=estimatedTime;}
-
-    public LocalDateTime getGeneratedAt(){ return generatedAt;}
-    public void setGeneratedAt(LocalDateTime generatedAt){ this.generatedAt=generatedAt;}
-
-    public Shipment getShipment(){ return shipment;}
-    public void setShipment(Shipment shipment){ this.shipment=shipment;}
 }
