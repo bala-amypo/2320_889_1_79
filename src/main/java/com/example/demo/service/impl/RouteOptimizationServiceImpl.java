c00ff060fdebc332868b599c97bf0ddcb28c5f1b
bala-amypo <bala@amypo.in>
2025-12-24 20:47:11 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RouteOptimizationServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RouteOptimizationServiceImpl.java
@@ -2,8 +2,8 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.RouteOptimizationResult;
 import com.example.demo.entity.Shipment;
-import com.example.demo.repository.RouteOptimizationResultRepository;
 import com.example.demo.repository.ShipmentRepository;
+import com.example.demo.repository.RouteOptimizationResultRepository;
 import com.example.demo.service.RouteOptimizationService;
 import org.springframework.stereotype.Service;
 
@@ -15,32 +15,28 @@ public class RouteOptimizationServiceImpl implements RouteOptimizationService {
     private final ShipmentRepository shipmentRepository;
     private final RouteOptimizationResultRepository resultRepository;
 
-    public RouteOptimizationServiceImpl(
-            ShipmentRepository shipmentRepository,
-            RouteOptimizationResultRepository resultRepository
-    ) {
+    public RouteOptimizationServiceImpl(ShipmentRepository shipmentRepository,
+                                        RouteOptimizationResultRepository resultRepository) {
         this.shipmentRepository = shipmentRepository;
         this.resultRepository = resultRepository;
     }
 
     @Override
-    public boolean optimizeRoute(Long shipmentId) {
+    public RouteOptimizationResult optimizeRoute(Long shipmentId) {
 
         Shipment shipment = shipmentRepository.findById(shipmentId)
-                .orElse(null);
+                .orElseThrow(() -> new RuntimeException("Shipment Not Found"));
 
-        if (shipment == null) {
-            return false;
-        }
+        RouteOptimizationResult result =
+                resultRepository.findByShipmentId(shipmentId)
+                        .orElse(new RouteOptimizationResult());
 
-        RouteOptimizationResult result = new RouteOptimizationResult();
         result.setShipment(shipment);
         result.setTotalDistance(120.0);
         result.setEstimatedTime(2.5);
         result.setGeneratedAt(LocalDateTime.now());
 
-        resultRepository.save(result);
-        return true;
+        return resultRepository.save(result);
     }
 
     @Override
