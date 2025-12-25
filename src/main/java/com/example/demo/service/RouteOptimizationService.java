946f8d118101bf4d86d723c8157e26a5678eaabe
bala-amypo <bala@amypo.in>
2025-12-25 05:47:35 +0000

    User commit

--- src/main/java/com/example/demo/service/RouteOptimizationService.java
+++ src/main/java/com/example/demo/service/RouteOptimizationService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 import com.example.demo.entity.RouteOptimizationResult;
 
 public interface RouteOptimizationService {
-    RouteOptimizationResult optimize(Long shipmentId);
+
+    RouteOptimizationResult optimizeRoute(Long shipmentId);
+
     RouteOptimizationResult getResult(Long shipmentId);
 }
