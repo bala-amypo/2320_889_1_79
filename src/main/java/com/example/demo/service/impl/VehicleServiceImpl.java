31e5dc1ac2c9f86e7590e4e3b5e0d7f4d976812d
bala-amypo <bala@amypo.in>
2025-12-25 05:29:35 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,51 +1,16 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.entity.User;
-import com.example.demo.entity.Vehicle;
-import com.example.demo.repository.UserRepository;
-import com.example.demo.repository.VehicleRepository;
-import com.example.demo.service.VehicleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
-    @Autowired
-    private VehicleRepository vehicleRepo;
+    private final VehicleRepository vehicleRepo;
+    private final UserRepository userRepo;
 
-    @Autowired
-    private UserRepository userRepo;
-
-    @Override
-    public Vehicle save(Vehicle vehicle) {
-        return vehicleRepo.save(vehicle);
+    public VehicleServiceImpl(VehicleRepository v, UserRepository u){
+        this.vehicleRepo = v;
+        this.userRepo = u;
     }
 
-    @Override
-    public List<Vehicle> getAll() {
-        return vehicleRepo.findAll();
-    }
-
-    @Override
-    public Optional<Vehicle> findById(long id) {
-        return vehicleRepo.findById(id);
-    }
-
-    @Override
-    public Vehicle addVehicle(Long userId, Vehicle vehicle) {
-        User user = userRepo.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-
-        vehicle.setUser(user);
-        return vehicleRepo.save(vehicle);
-    }
-
-    @Override
-    public List<Vehicle> getVehiclesByUser(Long userId) {
-        return vehicleRepo.findByUserId(userId);
+    public VehicleServiceImpl() {
+        this.vehicleRepo = null;
+        this.userRepo = null;
     }
 }
