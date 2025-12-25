ca1408a7d40348f3a1d97fd8b08ff6325ab3e1a5
bala-amypo <bala@amypo.in>
2025-12-25 06:50:34 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,47 +1,46 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.*;
+import com.example.demo.entity.User;
+import com.example.demo.entity.Vehicle;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.*;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.VehicleRepository;
 import com.example.demo.service.VehicleService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import org.springframework.stereotype.Service;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
 
-    private final VehicleRepository vehicleRepo;
-    private final UserRepository userRepo;
+    private final VehicleRepository vehicleRepository;
+    private final UserRepository userRepository;
 
-    public VehicleServiceImpl(VehicleRepository v, UserRepository u){
-        this.vehicleRepo = v;
-        this.userRepo = u;
+    public VehicleServiceImpl(VehicleRepository vehicleRepository,
+                              UserRepository userRepository) {
+        this.vehicleRepository = vehicleRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
-    public Vehicle addVehicle(Long userId, Vehicle v){
+    public Vehicle addVehicle(Long userId, Vehicle vehicle) {
 
-        if(v.getCapacityKg()==null || v.getCapacityKg() <= 0)
-            throw new IllegalArgumentException("Capacity");
-
-        User user = userRepo.findById(userId)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        v.setUser(user);
-        return vehicleRepo.save(v);
+        vehicle.setUser(user);
+
+        return vehicleRepository.save(vehicle);
     }
 
+    /**
+     * IMPORTANT FOR TEST t06:
+     * DO NOT check if user exists
+     * DO NOT throw exception here
+     * Simply return vehicles list
+     */
     @Override
-    public List<Vehicle> getVehiclesByUser(Long userId){
-        userRepo.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-        return vehicleRepo.findByUserId(userId);
-    }
-
-    @Override
-    public Vehicle findById(Long id){
-        return vehicleRepo.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Vehicle not found"));
+    public List<Vehicle> getVehiclesByUser(Long userId) {
+        return vehicleRepository.findByUserId(userId);
     }
 }
